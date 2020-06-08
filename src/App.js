03b55b23b5232e62af63057fc218fbfb06518f4b
MohammadAli <aaaa@aaa.ir>
2020-06-08 12:18:23 +0430

    Create shop page and Collection Preview Part 1

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import { Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from './pages/shop/shop.component'
 
 const HatPage = (props) => (
-  
+
   <div>
-  <Link to='/'>home</Link>
+    <Link to='/'>home</Link>
     <h1>HATS PAGE</h1>
   </div>
 );
 
 function App() {
-  
+
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
