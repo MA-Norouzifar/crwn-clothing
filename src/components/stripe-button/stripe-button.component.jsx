be8918becb4935490072b0125761712b2ab22ff5
Anformatic <Anformatic@nezam.com>
2020-07-07 13:07:04 +0430

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishablekey =
+    "pk_test_51H2BJ4EiAMLiYxfE8OcwOSI8U60hys08k3UBJulgIn3RooCZNaolzgEvU8u27f8nlYrMRLRnpejQiW2ggYpxpWrp00toFvUVoW";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`You total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishablekey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
