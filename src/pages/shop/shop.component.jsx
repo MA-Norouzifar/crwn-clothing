3a0310968bb09d60cabff1b8115d1267fa5ec51a
Anformatic <Anformatic@nezam.com>
2020-07-13 11:16:36 +0430

    use redux tunk and solve problem is it

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
-
+//selectIsCollectionsLoaded == some times when we select a collection
+// and refresh a page we get a error 'title' not define
+//because defualt value is null for colection for this reasone  we create new selector
+// we can use UNSAFE_componentWillMount() to but is unsafe
 import {
-  firestore,
-  convertcollectionsnapshotToMap,
-} from "../../firebase/firebase.utils";
+  selectIsCollectionFetching,
+  selectIsCollectionsLoaded,
+} from "../../redux/shop/shop.selectors";
+// import {
+//   firestore,
+//   convertcollectionsnapshotToMap,
+// } from "../../firebase/firebase.utils";
 
-import { updateCollections } from "../../redux/shop/shop.actions";
+//import { updateCollections } from "../../redux/shop/shop.actions";
+import { fetchCollectionsAsync } from "../../redux/shop/shop.actions";
 
 import WithSpinner from "../../components/with-spinner/with-spinner.component";
 
@@ -17,25 +26,35 @@ const CollectionOverViewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
-  state = {
-    loading: true,
-  };
+  // UNSAFE_componentWillMount(){
 
-  unsubscribeFromSnapsshop = null;
+  // }
+  // state = {
+  //   loading: true,
+  // };
+
+  // unsubscribeFromSnapsshop = null;
 
   componentDidMount() {
-    const { updateCollections } = this.props;
-    const collectionRef = firestore.collection("collections");
+    //Redux Thunk
+    const { fetchCollectionsAsync } = this.props;
+    fetchCollectionsAsync();
+    // const { updateCollections } = this.props;
+    // const collectionRef = firestore.collection("collections");
+    //rest API
+    // fetch(
+    //   "https://firestore.googleapis.com/v1/projects/crwn-db-a5591/databases/(default)/documents/connections"
+    // )
+    //   .then((respone) => respone.json())
+    //   .then((collections) => console.log(collections));
     // permise pattern
-
-    collectionRef.get().then((snapshop) => {
-      //console.log({'snapshop':snapshop});
-      const collectionMap = convertcollectionsnapshotToMap(snapshop);
-      //console.log(collectionMap);
-      updateCollections(collectionMap);
-      this.setState({ loading: false });
-    });
-
+    // collectionRef.get().then((snapshop) => {
+    //   //console.log({'snapshop':snapshop});
+    //   const collectionMap = convertcollectionsnapshotToMap(snapshop);
+    //   //console.log(collectionMap);
+    //   updateCollections(collectionMap);
+    //   this.setState({ loading: false });
+    // });
     //observable pattern
     // this.unsubscribeFromSnapsshop = collectionRef.onSnapshot(
     //   async (snapshop) => {
@@ -49,22 +68,30 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    const { match } = this.props;
-    const { loading } = this.state;
+    const { match, isCollectionFetching, isCollectionsLoaded } = this.props;
+    //const { loading } = this.state;
     return (
       <div className="shop-page">
         <Route
           exact
           path={`${match.path}`}
           render={(props) => (
-            <CollectionOverViewWithSpinner isLoading={loading} {...props} />
+            <CollectionOverViewWithSpinner
+              isLoading={isCollectionFetching}
+              {...props}
+            />
+            // <CollectionOverViewWithSpinner isLoading={loading} {...props} />
           )}
         />
         {/* <Route exact path={`${match.path}`} component={CollectionsOverview} /> */}
         <Route
           path={`${match.path}/:collectionId`}
           render={(props) => (
-            <CollectionPageWithSpinner isLoading={loading} {...props} />
+            <CollectionPageWithSpinner
+              isLoading={!isCollectionsLoaded}
+              {...props}
+            />
+            // <CollectionPageWithSpinner isLoading={loading} {...props} />
           )}
         />
         {/* <Route
@@ -76,9 +103,16 @@ class ShopPage extends React.Component {
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  updateCollections: (collectionsMap) =>
-    dispatch(updateCollections(collectionsMap)),
+const mapStateToProps = createStructuredSelector({
+  isCollectionFetching: selectIsCollectionFetching,
+  isCollectionsLoaded: selectIsCollectionsLoaded,
 });
+const mapDispatchToProps = (dispatch) => ({
+  fetchCollectionsAsync: () => dispatch(fetchCollectionsAsync()),
+});
+// const mapDispatchToProps = (dispatch) => ({
+//   updateCollections: (collectionsMap) =>
+//     dispatch(updateCollections(collectionsMap)),
+// });
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -3,15 +3,34 @@ import shopActionTypes from "./shop.types";
 
 const INITIAL_STATE = {
   collections: null,
+  isFetching:false,
+  errorMessage:undefined
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case shopActionTypes.UPDATE_COLLECTIONS:
-      return {
+    case shopActionTypes.FETCH_COLLECTION_START:
+      return{
         ...state,
-        collections: action.payload,
-      };
+        isFetching:true
+      }
+    case shopActionTypes.FETCH_COLLECTION_SUCCES:
+        return{
+          ...state,
+          isFetching:false,
+          collections:action.payload
+        }  
+    case shopActionTypes.FETCH_COLLECTION_FAILURE:
+        return{
+          ...state,
+          isFetching:false,
+          errorMessage:action.payload
+        }  
+    // case shopActionTypes.UPDATE_COLLECTIONS:
+    //   return {
+    //     ...state,
+    //     collections: action.payload,
+    //   };
     default:
       return state;
   }
